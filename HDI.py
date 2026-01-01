1f0b7e2f266658915ca84d0bb03698b30ac7ad65
Vladislaus-37 <axi.studios@yandex.ru>
2026-01-01 09:23:54 +0300

    Update HDI.py to beta 9
    1. You can use the PY command to execute arbitrary code:
        PY EXE [string] | [string] - execute python strings
        PY FILE [name] - execute python file
    2. Fix small bugs

--- HDI.py
+++ HDI.py
@@ -17,13 +17,13 @@ file.close()
 def main():
     # Output Header
     print(colorama.Fore.WHITE+colorama.Back.BLACK+' ')
-    print('HDI β8')
+    print('HDI β9')
     print(colorama.Style.RESET_ALL+'—————————')
 
 
     # Main part
     while True:
-        cow=input('$').strip().split(" ")
+        cow=input(colorama.Fore.YELLOW+str(os.path.basename(os.getcwd()))+'>'+colorama.Fore.WHITE+' $').strip().split(" ")
         Cow_Herder(cow)
 
 def Cow_Herder(cow_list):
@@ -34,7 +34,7 @@ def Cow_Herder(cow_list):
         read(cow_list[1:])
 
     elif cow_list[0]=='open': # Open file with default application
-        runfile(cow_list[1:])
+        runfile(cow_list[1:])   
 
     elif cow_list[0]=='chdir' or cow_list[0]=='cd': # Change dir
         chdir(cow_list[1:])
@@ -60,6 +60,7 @@ def Cow_Herder(cow_list):
         print('  about                     - About HDI')
         print('  log     [text]            - Log text to console')
         print('  fms     [name]            - Run FMS script')
+        print('  py      [type] [name/str] - Run python file or code')
 
     elif cow_list[0]=='delfile': # Delete file
         delfile(cow_list[1:])
@@ -104,9 +105,14 @@ def Cow_Herder(cow_list):
         print(colorama.Fore.LIGHTBLUE_EX+' '.join(cow_list[1:]).rstrip('\n'), colorama.Fore.WHITE)
     
     elif cow_list[0]=='fms':
-            fms(cow_list[1])
+        fms(cow_list[1])
+
+    elif cow_list[0]=='py':
+        pyexe(cow_list[1:])
+
     elif cow_list[0]=='' or cow_list[0]=='\n' or cow_list[0]=='#':
         123
+
     else: # This SYNTAX else)
         print(colorama.Fore.LIGHTRED_EX+' ')
         print('SYNTAX_ERR')
@@ -248,6 +254,20 @@ def fms(path):
         print(colorama.Fore.LIGHTRED_EX + '    FSM_ERR')
         print(colorama.Fore.WHITE + '    May be name of script is invalid')
         print('    Or script will be wrong')
+
+def pyexe(pyexe_pe):
+    if pyexe_pe[0] == 'file':
+        runfile([' '.join((pyexe_pe[1:]))])
+    elif pyexe_pe[0] == 'exe':
+        code_py=' '.join(pyexe_pe[1:]).split(' | ')
+        try:
+            for i in code_py:
+                exec(i)
+        except Exception as err:
+            print(colorama.Fore.LIGHTRED_EX + '    PY_ERR')
+            print(colorama.Fore.MAGENTA + '    ' + str(err))
+            print(colorama.Fore.WHITE + '    Check your code')
+    return 0
 
 # Run Part  
 if sys.argv[-1]!=sys.argv[0]:
